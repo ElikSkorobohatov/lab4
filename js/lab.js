d3c23d693e5748b3b155b8ab698039c7239ab348
Elik Skorobohatov <ee.sd.2005@gmail.com>
2024-05-13 01:39:19 +0300

    second variant

--- js/lab.js
+++ js/lab.js
@@ -1,9 +1,54 @@
 const addNote  = document.querySelector('.btn');
 const moveNote = document.querySelector('.area');
+const closeNote = document.querySelector('.close');
+
+let notes = [];
+
+
+function notesRenderer(notesList) {
+    let template = '';
+    notesList.forEach(function (note, index) {
+        template += '<div class="block">'+
+                        '<div class="up">'+
+                            '<h3>Title</h3>'+
+                            '<a class="close">X</a>'+
+                        '</div>'+
+                        '<div class="general">'+
+                            '<p>Тут може бути Ваша реклама! :)</p>'+
+                        '</div>'+
+                    '</div>';
+    });
+    moveNote.innerHTML = template;
+    console.log(notes);
+}
+
+function notesRemove(notesList) {
+    let template = '';
+    notesList.forEach(function (note, index) {
+        template -= '<div class="block">'+
+                        '<div class="up">'+
+                            '<h3>Title</h3>'+
+                            '<a class="close">X</a>'+
+                        '</div>'+
+                        '<div class="general">'+
+                            '<p>Тут може бути Ваша реклама! :)</p>'+
+                        '</div>'+
+                    '</div>';
+    });
+    moveNote.innerHTML = template;
+    console.log(notes);
+}
 
 addNote.addEventListener('click', function(){
-    console.log('click')
-})
+    notes.push({
+        coords: {
+            x: 0,
+            y: 0
+        },
+        text: ''
+    });
+    notesRenderer(notes);
+});
 
 moveNote.addEventListener('mouseup', function(){
     console.log('mouseup')
@@ -13,3 +58,10 @@ moveNote.addEventListener('mousedown', function(){
     console.log('mousedown')
 })
 
+moveNote.addEventListener('mousemove', function(){
+    console.log('mousemove')
+})
+
+closeNote.addEventListener('click', function(){
+    notesRemove(notes);
+})
